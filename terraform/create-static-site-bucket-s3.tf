2d52ff702af6efdeb0eaa6bb97befe2236f580f6
jacksonPrimo <jackson.primodev@gmail.com>
2026-03-30 19:41:21 -0300

    improve sintaxe and add public access policy

--- terraform/create-static-site-bucket-s3.tf
+++ terraform/create-static-site-bucket-s3.tf
@@ -3,45 +3,60 @@ provider "aws" {
 }
 
 variable "bucket_name" {
-  type        = string
+  type    = string
 }
 
 resource "aws_s3_bucket" "static_site_bucket" {
   bucket = "static-site-${var.bucket_name}"
 
-  website {
-    index_document = "index.html"
-    error_document = "404.html"
-  }
-
   tags = {
-    Name = "Static Site Bucket"
+    Name        = "Static Site Bucket"
     Environment = "Production"
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "static_site_bucket" {
+resource "aws_s3_bucket_website_configuration" "static_site_config" {
   bucket = aws_s3_bucket.static_site_bucket.id
 
-  block_public_acls = false
-  block_public_policy = false
-  ignore_public_acls = false
-  restrict_public_buckets = false
-}
+  index_document {
+    suffix = "index.html"
+  }
 
-resource "aws_s3_bucket_ownership_controls" "static_site_bucket" {
-  bucket = aws_s3_bucket.static_site_bucket.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
+  error_document {
+    key = "404.html"
   }
 }
 
-resource "aws_s3_bucket_acl" "static_site_bucket" {
-  depends_on = [
-    aws_s3_bucket_public_access_block.static_site_bucket,
-    aws_s3_bucket_ownership_controls.static_site_bucket
-  ]
+# 3. Remover bloqueios de acesso público (Obrigatório para a Policy funcionar)
+resource "aws_s3_bucket_public_access_block" "static_site_bucket" {
+  bucket = aws_s3_bucket.static_site_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "allow_public_access" {
+  depends_on = [aws_s3_bucket_public_access_block.static_site_bucket]
 
   bucket = aws_s3_bucket.static_site_bucket.id
-  acl = "public-read"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.static_site_bucket.arn}/*"
+      },
+    ]
+  })
+}
+
+output "website_url" {
+  value = aws_s3_bucket_website_configuration.static_site_config.website_endpoint
+  description = "A URL pública do seu site"
 }
